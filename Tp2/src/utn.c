922e99ff66345179b6bbf936ac6ebab32397c9d5
unknown <juan.sueldo1989@gmail.com>
2021-05-11 19:45:24 -0300

    Tp2 Segundo Commit

--- Tp2/src/utn.c
+++ Tp2/src/utn.c
@@ -289,4 +289,97 @@ int utn_getMayusMin (char name[], int tam)
 
    return 0;
 }
+//*** VALIDACIONES - INICIO
+int Validate_OnlyNumberInt(char *pData) {
+	int rtn = 1;
 
+	if (strlen(pData) > 0) {
+		for (int i = 0; i < strlen(pData); i++) {
+			if (isdigit(pData[i]) == 0) {
+				if (i == 0) {
+					if (pData[0] != '-') {
+						rtn = 0;
+						break;
+					}
+				} else {
+					rtn = 0;
+					break;
+				}
+			}
+		}
+	} else {
+		rtn = 0;
+	}
+
+	return rtn;
+}
+
+int Validate_OnlyNumberFloat(char *pData) {
+	int countSigno = 0;
+	int rtn = 1;
+
+	if (strlen(pData) > 0) {
+		for (int i = 0; i < strlen(pData); i++) {
+
+			if (pData[i] == '.' || pData[i] == ',') {
+				countSigno++;
+			} else {
+				if (isdigit(pData[i]) == 0) {
+					if (i == 0) {
+						if (pData[0] != '-') {
+							rtn = 0;
+							break;
+						}
+					} else {
+						rtn = 0;
+						break;
+					}
+				}
+			}
+		}
+
+		if (countSigno > 1) {
+			rtn = 0;
+		}
+	} else {
+		rtn = 0;
+	}
+
+	return rtn;
+}
+
+int Validate_OnlyAlphabet(char *pData) {
+	int rtn = 1;
+
+	if (strlen(pData) > 0) {
+		for (int i = 0; i < strlen(pData); i++) {
+			if (isalpha(pData[i]) == 0) {
+				rtn = 0;
+				break;
+			}
+		}
+	} else {
+		rtn = 0;
+	}
+
+	return rtn;
+}
+
+int Validate_OnlyAlphabetWithSpaces(char *pData) {
+	int rtn = 1;
+
+	if (strlen(pData) > 0) {
+		for (int i = 0; i < strlen(pData); i++) {
+			if (isalpha(pData[i]) == 0) {
+				if (isspace(pData[i]) == 0) {
+					rtn = 0;
+					break;
+				}
+			}
+		}
+	} else {
+		rtn = 0;
+	}
+
+	return rtn;
+}
